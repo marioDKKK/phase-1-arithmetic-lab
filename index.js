aee7873280e51e986e2bbb076251ece9a8df1d4d
mario <kinuthiamariodavis@gmail.com>
2023-09-23 14:55:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1= 31;
+let num2 = 2;
+
+const multiply = num1*num2;
+
+
+   const random= Math.floor(Math.random()*10)+1;
+   let num3=19;
+   let num4=5;
+   const mod = num3%num4;
+
+   const max = Math.max(2,14,20,15,19);
